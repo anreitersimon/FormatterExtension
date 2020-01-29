2768168cfacabc27f9c656b57f224e722f52182b
simonbility <simon.anreiter@i-mobility.at>
2020-01-29 21:40:57 +0100

    Initial Commit

---
+++ FormatterExtension/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FormatterExtension
+//
+//  Created by Simon Anreiter on 29.01.20.
+//  Copyright © 2020 Simon Anreiter. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
